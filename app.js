d16b5bf3a757d1d3526bbec9f6e274b969384b96
deah <deah@MacBook-Pro-de-deah.local>
2024-04-08 14:12:49 -0500

    Mi primer cambio

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const pedidos = require('./routes/pedidos');
 
 const path = __dirname + '/views/';
-const port = 8080;
+//const port = 8080;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 app.use('/pedidos', pedidos);
 
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log(`Example app listening on ${port}!`);
+});
+
